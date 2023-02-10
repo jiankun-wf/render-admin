36943ea347ad49ad8308f2db841da639494bd9c5
wangjiankun <wangjiankun_moreji@163.com>
2023-02-10 17:06:56 +0800

    feat: code

--- src/views/dashboard/console/components/Code.vue
+++ src/views/dashboard/console/components/Code.vue
@@ -6,81 +6,43 @@
       size="small"
       :bordered="false"
     >
-      <n-code :code="code" language="typescript" />
+      <n-code :code="code" language="typescript" :show-line-numbers="true" />
     </n-card>
   </div>
 </template>
 
 <script setup lang="ts">
   const code = `
-  import type {
-  ComponentRenderProxy,
-  VNode,
-  VNodeChild,
-  ComponentPublicInstance,
-  FunctionalComponent,
-  PropType as VuePropType,
-} from 'vue';
-
-declare global {
-  // vue
-  declare type PropType<T> = VuePropType<T>;
-  declare type VueNode = VNodeChild | JSX.Element;
-
-  export type Writable<T> = {
-    -readonly [P in keyof T]: T[P];
-  };
-
-  declare type Nullable<T> = T | null;
-  declare type NonNullable<T> = T extends null | undefined ? never : T;
-  declare type Recordable<T = any> = Record<string, T>;
-  declare type ReadonlyRecordable<T = any> = {
-    readonly [key: string]: T;
-  };
-  declare type Indexable<T = any> = {
-    [key: string]: T;
-  };
-  declare type DeepPartial<T> = {
-    [P in keyof T]?: DeepPartial<T[P]>;
-  };
-  declare type TimeoutHandle = ReturnType<typeof setTimeout>;
-  declare type IntervalHandle = ReturnType<typeof setInterval>;
-
-  declare interface ChangeEvent extends Event {
-    target: HTMLInputElement;
-  }
-
-  declare interface WheelEvent {
-    path?: EventTarget[];
-  }
-
-  interface ImportMetaEnv extends ViteEnv {
-    __: unknown;
-  }
-
-  declare function parseInt(s: string | number, radix?: number): number;
-
-  declare function parseFloat(string: string | number): number;
-
-  namespace JSX {
-    // tslint:disable no-empty-interface
-    type Element = VNode;
-    // tslint:disable no-empty-interface
-    type ElementClass = ComponentRenderProxy;
-
-    interface ElementAttributesProperty {
-      $props: any;
-    }
-
-    interface IntrinsicElements {
-      [elem: string]: any;
-    }
-
-    interface IntrinsicAttributes {
-      [elem: string]: any;
-    }
-  }
+import CssRender from 'css-render';
+    
+const { c } = CssRender();    
+const style = c('body', ({ props }) => ({
+  margin: 0,
+  backgroundColor: props.backgroundColor
+}), [
+  c('&.dark', {
+    backgroundColor: 'black'
+  }),
+  c('.container', {
+    width: '100%'
+  })
+])
+// in onMounted
+style.mount()
+// in onUnMounted
+style.unmount()
+// parse result
+body {
+  margin: 0;
+  background-color: white;
 }
 
+body.dark {
+  background-color: black;
+}
+
+body .container {
+  width: 100%;
+}
   `;
 </script>
