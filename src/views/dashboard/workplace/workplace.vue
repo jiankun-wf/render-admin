ed2554e3e70488bbd6044753aab9f0d4369a4943
wangjiankun <wangjiankun_moreji@163.com>
2023-02-10 15:51:42 +0800

    fix: types 页面删除

--- src/views/dashboard/workplace/workplace.vue
+++ src/views/dashboard/workplace/workplace.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <n-alert title="嗨~ 你好" type="info" closable> 我刚才躲起来辣~ </n-alert>
-    <n-el tag="span" style="color: var(--primary-color)"> 123 </n-el>
-
-    <div class="fixed inset-0 z-50 bg-black bg-opacity-40">
-      <div class="fixed w-10 h-10 bg-white rounded-full top-10 left-10"></div>
-    </div>
   </div>
 </template>
 
